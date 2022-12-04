e566e0cbe187dca9ad72f7c3264cc97f67bb41e4
BOTScarlett <duongtruongtho12@gmail.com>
2022-12-04 17:04:36 +0700

    init project

---
+++ src/config/db/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
+
+connect = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://test:test@cluster0.bkjuojr.mongodb.net/test?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+    console.log('Connect susscessfully!!');
+  } catch (error) {
+    console.log('Connect fail!');
+  }
+}
+
+module.exports = { connect }
