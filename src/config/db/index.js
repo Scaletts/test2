3e125612a0c99c23327d6f919f90a838b8edde00
tonlongchau <tonlongchau@gmail.com>
2022-12-09 23:47:09 +0700

    fix text

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,19 +1,22 @@
-const mongoose = require('mongoose');
-const slug = require('mongoose-slug-generator');
-mongoose.plugin(slug);
+const mongoose = require('mongoose')
+const slug = require('mongoose-slug-generator')
+mongoose.plugin(slug)
 
 connect = async () => {
-  try {
-    await mongoose.connect('mongodb+srv://test:test@cluster0.bkjuojr.mongodb.net/test?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
-    });
-    console.log('Connect susscessfully!!');
-  } catch (error) {
-    console.log('Connect fail!');
-  }
+	try {
+		await mongoose.connect(
+			'mongodb+srv://test:test@cluster0.bkjuojr.mongodb.net/test?retryWrites=true&w=majority',
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useFindAndModify: false,
+				useCreateIndex: true,
+			}
+		)
+		console.log('Connect susscessfully!!')
+	} catch (error) {
+		console.log('Connect failure!')
+	}
 }
 
 module.exports = { connect }
