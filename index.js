9a2efc1876c69685d3424cc9f75bd17438dacb37
tonlongchau <tonlongchau@gmail.com>
2022-12-09 23:42:48 +0700

    update resource

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const { static } = require('express');
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const { static } = require('express')
+const express = require('express')
+const app = express()
+const PORT = 8888
 
-const path = require('path');
+const path = require('path')
 
-const db = require('./src/config/db/index');
-const route = require('./src/routes/index');
+const db = require('./src/config/db/index')
+const route = require('./src/routes/index')
 
 // SET view
 app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'src','resource', 'views'))
+app.set('views', path.join(__dirname, 'src', 'resource', 'views'))
 
 // static file
 app.use(express.static(path.join(__dirname, 'src', 'public')))
 
 // body-parser
-app.use(express.urlencoded(
-  {
-    extended: true
-  }
-))
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+)
 app.use(express.json())
-
 
 // override using a query value
 var methodOverride = require('method-override')
@@ -30,10 +29,11 @@ var methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
 // conect db
-db.connect();
+db.connect()
 
 // route init
-route(app);
+route(app)
 
-
-app.listen(PORT, ()=> console.log(`Server listening at http://localhost:${PORT}`))
+app.listen(PORT, () =>
+	console.log(`Server listening at http://localhost:${PORT}`)
+)
